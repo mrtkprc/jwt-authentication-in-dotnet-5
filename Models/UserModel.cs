860cc30201f01c330b9d64d618ce2690f1ec1be1
Mert Koprucu <mrtkprc@gmail.com>
2021-06-29 06:50:31 +0300

    UserModel introduced.

---
+++ Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_authentication_in_dotnet_5.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+
+
+
+    }
+}
