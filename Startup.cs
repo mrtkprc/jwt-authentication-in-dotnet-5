f1ef29bfcc835853d0f17f24df319f8fb3247db5
Mert Koprucu <mrtkprc@gmail.com>
2021-06-29 06:53:14 +0300

    Cors policy added.

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,10 @@ namespace jwt_authentication_in_dotnet_5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials().Build());
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
